6132fe462bb3ad8f5c9dd6a7a897d34f6e9aad19
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-05-06 19:23:13 +0200

    make SideBar responsive

--- src/Components/1_sidebar/SideBar.jsx
+++ src/Components/1_sidebar/SideBar.jsx
@@ -8,7 +8,7 @@ export default function SideBar() {
         setIsOpen(!isOpen);
     };
     return (
-        <div className={`${isOpen ? "w-64": "w-0"} md:w-64 bg-[#925FE2] md:rounded-3xl p-10 text-[#c6bbd9] flex flex-col items-center gap-5 min-h-[90vh] capitalize transition-all duration-300 overflow-hidden`}>
+        <div className={`${isOpen ? "w-64" : "w-0"} md:w-64 bg-[#925FE2] md:rounded-3xl p-10 text-[#c6bbd9] flex flex-col items-center gap-5 min-h-[90vh] capitalize transition-all duration-300 overflow-hidden`}>
             <div className='flex flex-col gap-4 justify-between items-center'>
                 <div className='w-32 h-32'>
                     <img src={avatar} alt="avatar" className='w-full' />
@@ -16,9 +16,17 @@ export default function SideBar() {
                 <h2 className='text-xl font-bold'> Student Portal</h2>
                 {/* responsive button */}
                 <button onClick={handleMenu} className='block md:hidden'>
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 14">
-                    <path d="M16 2H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 0 1 0 2Z" />
-                </svg>
+                    {isOpen ?
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+
+                        :
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 14">
+                            <path d="M16 2H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Zm0 6H1a1 1 0 0 1 0-2h15a1 1 0 0 1 0 2Z" />
+                        </svg>
+                    }
+
                 </button>
             </div>
 
