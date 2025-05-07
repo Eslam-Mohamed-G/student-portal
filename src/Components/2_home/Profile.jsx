b95b7ed75edaa8a4489772858aa1fe12f7ab4dfe
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-05-07 23:01:07 +0200

    Add Arabic and English content to Home page

--- src/Components/2_home/Profile.jsx
+++ src/Components/2_home/Profile.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import avatar from "../../assets/avataaars.svg"
+import { useTranslation } from 'react-i18next';
+import '../../i18n';
 
 function Profile() {
+    const { t, i18n } = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng)
+        localStorage.setItem('i18nextLng', lng);
+    };
     return (
         <div className='container md:px-5'>
                 <div className="flex items-center bg-[#b5c2cf] rounded-2xl shadow-sm md:max-w-xl md:px-5 md:py-4">
                     <img className="object-cover w-24 md:w-36" src={avatar} alt='personal-img' />
                     <div className="flex flex-col justify-between p-4 leading-normal">
-                        <h5 className="text-xl font-bold tracking-tight text-[#153e6a]">Eslam mohamed gomaa</h5>
-                        <p className="font-normal text-[#153e6a]">id:1000140550</p>
-                        <p className="font-normal text-[#153e6a]">level:Fourth year</p>
+                        <h5 className="text-xl font-bold tracking-tight text-[#153e6a]">{t('personalInfo.title')}</h5>
+                        <p className="font-normal text-[#153e6a]">{t('personalInfo.id')}</p>
+                        <p className="font-normal text-[#153e6a]">{t('personalInfo.level.title')}: {t('personalInfo.level.content')}</p>
                         {/* <p className="font-normal text-[#153e6a]">Department:Computer Science</p> */}
                     </div>
                 </div>
@@ -31,26 +38,34 @@ function Profile() {
                         <tbody>
                             <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
+                                {t('personalInfo.studentName.title')}
                                 </th>
                                 <td className="px-6 py-4">
-                                    Silver
+                                {t('personalInfo.studentName.content')}
                                 </td>
                             </tr>
                             <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
+                                {t('personalInfo.fatherName.title')}
                                 </th>
                                 <td className="px-6 py-4">
-                                    Silver
+                                {t('personalInfo.fatherName.content')}
                                 </td>
                             </tr>
                             <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
+                                {t('personalInfo.level.title')}
                                 </th>
                                 <td className="px-6 py-4">
-                                    Silver
+                                {t('personalInfo.level.content')}
+                                </td>
+                            </tr>
+                            <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {t('personalInfo.grade.title')}
+                                </th>
+                                <td className="px-6 py-4">
+                                {t('personalInfo.grade.content')}
                                 </td>
                             </tr>
                         </tbody>
